7834c1b16f564a843de8050e85ed7ffb8c6037ec
razborovdb <104690270+razborovdb@users.noreply.github.com>
2023-03-09 21:16:03 -0500

    Updated backend

--- backend/cryptowallet/src/main/java/com/development/dependency/DaoModule.java
+++ backend/cryptowallet/src/main/java/com/development/dependency/DaoModule.java
@@ -1,7 +1,9 @@
 package com.development.dependency;
 
-import com.amazon.ata.aws.dynamodb.DynamoDbClientProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import dagger.Module;
 import dagger.Provides;
@@ -21,11 +23,23 @@ public class DaoModule {
     @Singleton
     @Provides
     public DynamoDBMapper provideDynamoDBMapper() {
-        return new DynamoDBMapper(DynamoDbClientProvider.getDynamoDBClient(getRegion()));
+        return new DynamoDBMapper(DaoModule.getDynamoDBClient(getRegion()));
     }
 
     private Regions getRegion() {
 
         return Regions.US_WEST_2;
+    }
+
+    public static AmazonDynamoDB getDynamoDBClient() {
+        return getDynamoDBClient(Regions.US_WEST_2);
+    }
+
+    public static AmazonDynamoDB getDynamoDBClient(Regions region) {
+        if (null == region) {
+            throw new IllegalArgumentException("region cannot be null");
+        } else {
+            return (AmazonDynamoDB)((AmazonDynamoDBClientBuilder)((AmazonDynamoDBClientBuilder)AmazonDynamoDBClientBuilder.standard().withCredentials(DefaultAWSCredentialsProviderChain.getInstance())).withRegion(region)).build();
+        }
     }
 }
